6fcb65c3a7ed7f2693be1d7bfcc8a770521a31f9
uhammadziyo <DilafruzIminova71@gmail.com>
2024-08-20 10:25:41 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+let arr = ["Firdavs","Amir","Davron",`Zioy`];
+
+alert("sizning arryngizga ${arr.length}ta element bor");
+
+let salom = confirm(`malumot o'chrasizmi`);
+
+if(salom) {
+    arr.pop();
+}else{
+    arr.pop(jasur)
+}
+
+alert("siznig arraligizda ${arr.length}ta elemet bor");
